eaba1da74530af3c4864617b683b17fabfbcee2a
Scott Rothrock <scott@blackwhitedog.com>
2023-04-16 20:40:20 +0900

    fix: Convert tabs to spaces

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,23 @@
 
 deploy_target="../current/"
 exclude_from_deploy=(
-	"deploy.sh"
-	"LICENSE"
-	"README.md"
-	".git"
+    "deploy.sh"
+    "LICENSE"
+    "README.md"
+    ".git"
 )
 exclusion_args=""
 
 # Build exclusion string for rysnc
 for i in "${exclude_from_deploy[@]}"; do
-	exclusion_args+="--exclude $i "
+    exclusion_args+="--exclude $i "
 done
 
 echo "Deploying to $deploy_target"
 echo "Excluding: ${exclude_from_deploy[@]}"
 
 deploy() {
-	rsync -av --delete ./ $deploy_target $exclusion_args
+    rsync -av --delete ./ $deploy_target $exclusion_args
 }
 
 deploy
